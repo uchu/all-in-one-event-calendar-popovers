237ee83c8d884668d13f81684f42602cdbfbf68b
uchu <pavel@time.ly>
2015-03-23 20:25:34 +0530

    (#2) Moved all text out of the templates.

--- app/view/sas-frontend.php
+++ app/view/sas-frontend.php
@@ -17,7 +17,14 @@ class Ai1ecsas_Frontend extends Ai1ec_Base {
 	 */
 	public function add_buttons( $buttons ) {
 		$loader   = $this->_registry->get( 'theme.loader' );
-		$file     = $loader->get_file( 'buttons.twig', array(), false );
+		$file     = $loader->get_file(
+			'buttons.twig', array(
+				'text_save'  => __( 'Save', AI1ECSAS_PLUGIN_NAME ),
+				'text_saved' => __( 'Saved', AI1ECSAS_PLUGIN_NAME ),
+				'text_share' => __( 'Share', AI1ECSAS_PLUGIN_NAME )
+			),
+			false
+		);
 		$buttons .= $file->get_content();
 		return $buttons;
 	}
@@ -34,7 +41,10 @@ class Ai1ecsas_Frontend extends Ai1ec_Base {
 		$file     = $loader->get_file(
 			'toolbar-buttons.twig',
 			array(
-				'request' => $parser
+				'request'            => $parser,
+				'text_saved_events'  => __( 'Your Saved Events', AI1ECSAS_PLUGIN_NAME ),
+				'text_exit'          => __( 'Exit', AI1ECSAS_PLUGIN_NAME ),
+				'text_share'         => __( 'Share these Events', AI1ECSAS_PLUGIN_NAME )
 			),
 			false
 		);
@@ -49,7 +59,14 @@ class Ai1ecsas_Frontend extends Ai1ec_Base {
 	 */
 	public function add_clear_buttons( $buttons ) {
 		$loader   = $this->_registry->get( 'theme.loader' );
-		$file     = $loader->get_file( 'clear-buttons.twig', array(), false );
+		$file     = $loader->get_file(
+			'clear-buttons.twig',
+			array(
+				'text_clear_expired' => __( 'Clear Expired Events', AI1ECSAS_PLUGIN_NAME ),
+				'text_clear_all'     => __( 'Clear all Saved Events', AI1ECSAS_PLUGIN_NAME )
+			),
+			false
+		);
 		$buttons .= $file->get_content();
 		return $buttons;
 	}
@@ -61,7 +78,79 @@ class Ai1ecsas_Frontend extends Ai1ec_Base {
 	 */
 	public function add_share_modal( $html ) {
 		$loader = $this->_registry->get( 'theme.loader' );
-		$file   = $loader->get_file( 'share-modal.twig', array(), false );
+		$file   = $loader->get_file( 'share-modal.twig',
+			array(
+				'text_share_event'            => __(
+					'Share Event',
+					AI1ECSAS_PLUGIN_NAME
+				),
+				'text_multiple'               => __(
+					's',
+					AI1ECSAS_PLUGIN_NAME
+				),
+				'text_step_1'                 => __(
+					'Step 1.',
+					AI1ECSAS_PLUGIN_NAME
+				),
+				'text_step_2'                 => __(
+					'Step 2.',
+					AI1ECSAS_PLUGIN_NAME
+				),
+				'text_step_1_title'           => __(
+					'Give your custom calendar a title.',
+					AI1ECSAS_PLUGIN_NAME
+				),
+				'text_step_2_title'           => __(
+					'Share or bookmark this link or click on a button below:',
+					AI1ECSAS_PLUGIN_NAME
+				),
+				'text_custom_calendar'        => __(
+					'ex. Custom Calendar',
+					AI1ECSAS_PLUGIN_NAME
+				),
+				'text_shorten'                => __(
+					'Shorten',
+					AI1ECSAS_PLUGIN_NAME
+				),
+				'text_open'                   => __(
+					'Open',
+					AI1ECSAS_PLUGIN_NAME
+				),
+				'text_email'                  => __(
+					'Email',
+					AI1ECSAS_PLUGIN_NAME
+				),
+				'text_facebook'               => __(
+					'Facebook',
+					AI1ECSAS_PLUGIN_NAME
+				),
+				'text_twitter'                => __(
+					'Twitter',
+					AI1ECSAS_PLUGIN_NAME
+				),
+				'text_google_plus'            => __(
+					'Google+',
+					AI1ECSAS_PLUGIN_NAME
+				),
+				'text_check_out_these_events' => __(
+					'Check out these events',
+					AI1ECSAS_PLUGIN_NAME
+				),
+				'text_click_on_this_link'     => __(
+					'Click on this link to see some cool events:',
+					AI1ECSAS_PLUGIN_NAME
+				),
+				'text_enter_email'            => __(
+					'Enter email address',
+					AI1ECSAS_PLUGIN_NAME
+				),
+				'text_send'                   => __(
+					'Send',
+					AI1ECSAS_PLUGIN_NAME
+				)
+			),
+			false
+		);
 		$html  .= $file->get_content();
 		return $html;
 	}
