e1456b5488597a953ea81640d29bd408d5dd5d2d
uchu <pavel@time.ly>
2015-03-24 00:03:15 +0530

    (#2) Test - added a static flag for Share dialog.

--- app/view/sas-frontend.php
+++ app/view/sas-frontend.php
@@ -106,6 +106,10 @@ class Ai1ecsas_Frontend extends Ai1ec_Base {
 	 * @return string HTML for modal dialog.
 	 */
 	public function add_share_modal( $html = '' ) {
+		static $launched = null;
+		if ( $launched ) {
+			return $html;
+		}
 		$loader = $this->_registry->get( 'theme.loader' );
 		$file   = $loader->get_file( 'share-modal.twig',
 			array(
@@ -181,6 +185,7 @@ class Ai1ecsas_Frontend extends Ai1ec_Base {
 			false
 		);
 		$html  .= $file->get_content();
+		$launched = true;
 		return $html;
 	}
 
